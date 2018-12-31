e1c48fdfb51e9aaa2f77772a2e9b3f6cef18ee99
Michael Hu <pxmyhdev@gmail.com>
2018-12-31 14:11:40 -0500

    update image handling and remove app.js

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
+"use strict";
+
+// Imports dependencies and set up http server
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express().use(bodyParser.json()); // creates express http server
+const request = require("request");
+
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
+
+app.post("/webhook", (req, resp) => {
+  let body = req.body;
+
+  if (body.object === "page") {
+    body.entry.forEach(function(entry) {
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+    });
+
+    // Returns a '200 OK' response to all requests
+    resp.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    resp.sendStatus(404);
+  }
+});
 
 // Adds support for GET requests to our webhook
 app.get("/webhook", (req, res) => {
@@ -23,3 +65,85 @@ app.get("/webhook", (req, res) => {
     }
   }
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // check if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You sent the message: "${
+        received_message.text
+      }". Now send me an image!`
+    };
+  } else if (received_message.attachments) {
+    // Gets the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes"
+                },
+                {
+                  type: "postback",
+                  title: "No!",
+                  payload: "no"
+                }
+              ]
+            }
+          ]
+        }
+      }
+    };
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+  console.log("PostBack is still under development...");
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid
+    },
+    message: response
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}

25d488c62b6ef394b9ea970bee0bf0476f0eb89a
Michael Hu <pxmyhdev@gmail.com>
2018-12-31 13:28:01 -0500

    get psid and update gitignore

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-'use strict';
+"use strict";
 
 // Imports dependencies and set up http server
-const
-  express = require('express'),
-  bodyParser = require('body-parser'),
+const express = require("express"),
+  bodyParser = require("body-parser"),
   app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
-app.post('/webhook', (req, resp) => {
-
+app.post("/webhook", (req, resp) => {
   let body = req.body;
 
-  if (body.object === 'page') {
+  if (body.object === "page") {
     body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but 
+      // Gets the message. entry.messaging is an array, but
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
     });
 
     // Returns a '200 OK' response to all requests
-    resp.status(200).send('EVENT_RECEIVED');
+    resp.status(200).send("EVENT_RECEIVED");
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
     resp.sendStatus(404);
@@ -31,29 +32,25 @@ app.post('/webhook', (req, resp) => {
 });
 
 // Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
-
+app.get("/webhook", (req, res) => {
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "YrvzX5ID1RJiphbBDsYkfpSHIBySw3IR"
-    
+  let VERIFY_TOKEN = "YrvzX5ID1RJiphbBDsYkfpSHIBySw3IR";
+
   // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-  
     // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
       // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
+      console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
-    
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+      res.sendStatus(403);
     }
   }
 });

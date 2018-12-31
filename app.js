63a4a5b45af7df7934d6c6d44985ae95401bac29
Michael Hu <pxmyhdev@gmail.com>
2018-12-31 13:55:23 -0500

    add message handling and sending message back

--- app.js
+++ app.js
@@ -1,45 +1,25 @@
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
-
+app.get("/webhook", (req, res) => {
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "YrvzX5ID1RJiphbBDsYkfpSHIBySw3IR"
-    
+  let VERIFY_TOKEN = "YrvzX5ID1RJiphbBDsYkfpSHIBySw3IR";
+
   // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-  
     // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
       // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
+      console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
-    
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+      res.sendStatus(403);
     }
   }
 });
-
-
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
-
-}
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-  
-}
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 "use strict";
 
 // Imports dependencies and set up http server
-const express = require("express"),
-  bodyParser = require("body-parser"),
-  app = express().use(bodyParser.json()); // creates express http server
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express().use(bodyParser.json()); // creates express http server
+const request = require("request");
+
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
@@ -62,3 +65,54 @@ app.get("/webhook", (req, res) => {
     }
   }
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // check if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You sent the message: "${
+        received_message.text
+      }". Now send me an image!`
+    };
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+  console.log("PostBack is still under development...");
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid
+    },
+    message: response
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}
